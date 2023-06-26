abe8f88b9142c338f4bf17dd6e7331515f1ca1bc
kenharvey-delacruz <kenharvey.delacruz@gcash.com>
2023-06-26 13:31:54 +0800

    Day 2 files

---
+++ day2/account-service/src/main/java/com/apper/GetAccountResponse.java
@@ -0,0 +1,16 @@
+package com.apper;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class GetAccountResponse {
+    private String firstName;
+    private String lastName;
+    private Double balance;
+
+    private String username;
+    private LocalDateTime registrationDate;
+    private String accountId;
+}
